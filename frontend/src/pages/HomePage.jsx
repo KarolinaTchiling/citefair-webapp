feb0437515932c936c72735e273a0cb803edbb6e
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-08 00:28:35 -0400

    Crop logo

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,27 @@
+import '../App.css';
+import Textform from '../components/Textform';
+import Circle from '../components//Circle'; 
+
 const HomePage = () => {
     return (
-        <div>Homepage</div>
-    )
-}
+        <div>
+            <div className="header-with-circle">
+                <Circle num={1} />
+                <h1>Analyze your reference list</h1>
+            </div>
+            <div className="header-with-circle">
+                <Circle num={2} />
+                <h1>Diversify your research</h1>
+            </div>
+            <div className="header-with-circle">
+                <Circle num={3} />
+                <h1>Generate a Citation Diversity Statement</h1>
+            </div>
+            <br></br>
+
+            <Textform />
+        </div>
+    );
+};
+
 export default HomePage;
