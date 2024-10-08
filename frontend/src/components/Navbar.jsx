3e108ff62a6898da60ecaadfd6e858732cde7df6
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-07 22:00:32 -0400

    Created the UI for the navigation bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import '../App.css'; 
+import React from 'react';
+import logo from '../assets/logo.png';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div id="navLogo">
+                <img src={logo} alt="CiteFair Logo" />
+            </div>
+            <div id="navTitle">
+                CiteFair
+            </div>
+            <ul className="navLinks">
+                <li><a href="/">Home</a></li>
+                <li><a href="/resources">Resources</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </div>
+    );
+}
+  
+  export default Navbar;
