6b6576ded4fdffed162c981077d321a53a866d3c
Karolina Tchiling <kdt@my.yorku.ca>
2024-10-08 00:25:59 -0400

    Created circle components to be used on the HomePage

---
+++ frontend/src/components/Circle.jsx
@@ -0,0 +1,11 @@
+import './Circle.css'; 
+
+const Circle = ({num}) => {
+    return (
+      <div className="circle">
+        <span>{num}</span>
+      </div>
+    );
+  };
+  
+  export default Circle;
