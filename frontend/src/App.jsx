d8395c0160f44132c983a08e5752f88b8727aac4
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-18 14:35:07 -0500

    Initial connection with firebase and firebase realtime-db

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,49 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Homepage';
-import CreatePage from './pages/CreatePage';
-import Navbar from './components/Navbar';
+import React, { useEffect, useState } from "react";
+import cong from "../firebaseConfig"; // Assuming the correct path to your configuration file
+import { getDatabase, ref, onValue } from "firebase/database";
+
+// App.js
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Initialize the Firebase database with the provided configuration
+    const database = getDatabase(cong);
+    
+    // Reference to the specific collection in the database
+    const collectionRef = ref(database, "test");
+
+    // Function to fetch data from the database
+    const fetchData = () => {
+      // Listen for changes in the collection
+      onValue(collectionRef, (snapshot) => {
+        const dataItem = snapshot.val();
+
+        // Check if dataItem exists
+        if (dataItem) {
+          // Convert the object values into an array
+          const displayItem = Object.values(dataItem);
+          setData(displayItem);
+        }
+      });
+    };
+
+    // Fetch data when the component mounts
+    fetchData();
+  }, []);
+
   return (
-      <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/create" element={<CreatePage />} />
-      </Routes>
-    </>
+    <div>
+      <h1>Data from database:</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
 export default App;
+
