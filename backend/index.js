411ca26e11233515d57c8c0449d3b129efdcce08
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-19 01:36:42 -0500

    Added route to recieve frontend user call

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 // import "dotenv/config";
 import express from "express";
+import cors from "cors"; 
 import dataRoutes from "./routes/data.js";
 import userRoutes from "./routes/users.js";
 
@@ -10,6 +11,10 @@ const PORT = 5000;
 // Middleware to parse JSON
 app.use(express.json());
 
+app.use(cors({
+  origin: "http://localhost:5173", // Replace with your frontend's URL
+}));
+
 // Test Route
 app.get("/", (req, res) => {
   res.send("Hello, Node.js Backend!");
@@ -19,7 +24,7 @@ app.get("/", (req, res) => {
 app.use("/data", dataRoutes);
 
 // User routes
-app.use("/users", userRoutes);
+app.use("/api", userRoutes);
 
 
 // Start the server
