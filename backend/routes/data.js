c0e05066d0f36c94333c82cb44862f83d9413791
Karolina Tchiling <kdt@my.yorku.ca>
2024-11-18 19:16:34 -0500

    Users can upload files to the cloud db

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -5,7 +5,7 @@ const db = require("../firebase");
 // Route to get data from the "test" node
 router.get("/", async (req, res) => {
   try {
-    const ref = db.ref("test");
+    const ref = db.ref("users");
     ref.once("value", (snapshot) => {
       res.json(snapshot.val());
     });
