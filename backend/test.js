a1ce700c45491325f40492f97795e9ed77eb68ef
Karolina Tchiling <kdt@my.yorku.ca>
2024-12-03 00:45:28 -0500

    Implemented gender assignment with gender-api

---
+++ backend/test.js
@@ -0,0 +1,6 @@
+import { generate } from './cds_generator/generate.js';
+
+const testUserId = "cDskJiR0hqRQea0UNu3zZEpQ6np2"; // Replace with a valid user ID for Firebase
+// const testUserId = "O37o0MJa0OaSUoEAw7r4tX6GcFL2"
+
+generate(testUserId);
