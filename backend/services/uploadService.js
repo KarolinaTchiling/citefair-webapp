073f550727b9d3b1c16203572b1f8f5ce6fc0a59
Karolina Tchiling <kdt@my.yorku.ca>
2025-02-21 17:44:33 -0500

    Slight change to upload service

--- backend/services/uploadService.js
+++ backend/services/uploadService.js
@@ -2,7 +2,7 @@ import { bucket, db } from "../config/firebaseConfig.js";
 
 
 const uploadFileToFirebase = async (file, userId) => {
-    const fileName = `${Date.now()}_${file.originalname.replace(/[.#$/[\]]/g, "_")}`;
+    const fileName = `${file.originalname.replace(/[.#$/[\]]/g, "_")}`;
     const fileRef = bucket.file(`users/${userId}/uploads/${fileName}`);
 
     // Upload file to Firebase Storage
@@ -14,7 +14,7 @@ const uploadFileToFirebase = async (file, userId) => {
     const downloadURL = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(fileRef.name)}?alt=media`;
 
     // Save metadata to Firebase Realtime Database
-    await db.ref(`users/${userId}/files/${fileName}`).set({
+    await db.ref(`users/files/${fileName}`).set({
         url: downloadURL,
         originalFileName: file.originalname,
         uploadedAt: new Date().toISOString(),
