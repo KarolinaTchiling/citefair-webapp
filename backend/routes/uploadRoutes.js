bd857bc33172121942a9879b5c0391aa820c6d78
Karolina Tchiling <kdt@my.yorku.ca>
2025-04-03 15:59:33 -0400

    Added check to prevent file uplaod with the same name

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import { uploadFileToFirebase } from "../services/uploadService.js";
+import { db, bucket } from "../firebaseConfig.js";
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -14,6 +15,15 @@ router.post("/", upload.single("file"), async (req, res) => {
             return res.status(400).json({ error: "Missing user ID" });
         }
 
+        const normalizedFileName = req.file.originalname.replace(/\./g, "_");
+        const storagePath = `users/${userId}/uploads/${normalizedFileName}`;
+
+        const [files] = await bucket.getFiles({ prefix: storagePath });
+        const alreadyExists = files.some(file => file.name === storagePath);
+        if (alreadyExists) {
+            return res.status(409).json({ error: "A file with this name already exists." });
+        }
+
         const { fileName, downloadURL } = await uploadFileToFirebase(req.file, userId);
         res.json({ message: "File uploaded successfully", fileName, downloadURL });
     } catch (error) {
@@ -21,8 +31,6 @@ router.post("/", upload.single("file"), async (req, res) => {
         res.status(500).json({ error: "File upload failed" });
     }
 });
-
-
 
 
 export default router;
